7cf682eff9e6bf9b73998b714769c6ffefc3994f
LORNE ALEXANDRE <alexandre.lorne@hotmail.fr>
2020-01-06 18:43:50 +0100

    Maj login logout session

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from flask_wtf import FlaskForm
 from flask import Flask, flash, redirect, render_template, request, url_for,session
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from test.forms import *
-from test.models import *
-from flask_login import logout_user
+from test.models import User,feed,create_tables,drop_tables,database
+
 
 
 #----------------------------------#
@@ -22,10 +22,16 @@ app.secret_key="root"
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager._login_disabled = False
+
+
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
-
+    try:
+        return User.get(user_id)
+        print("ici fdp")
+    except :
+        return "va te faire enculer 2"
+    
 #------------------#
 
 @app.route('/')
@@ -47,15 +53,14 @@ def dashboard():
 @app.route('/add_feed', methods=['GET', 'POST'])
 @login_required
 def add_feed():
-    myfeed=feed()
     user_id = current_user.get_id() # return username in get_id()
-    print("",user_id)
+    myfeed=feed()
     form=FeedForm()
     form.user_feed=user_id
     if form.validate_on_submit():
         form.populate_obj(myfeed)
         myfeed.save()
-        flash('Hooray ! Boardgame created !')
+        flash('Your feed is save !!')
         return redirect(url_for('index'))
     return render_template('index.html',form=form)
 
@@ -65,17 +70,23 @@ def add_feed():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    print('ici')
     if form.validate_on_submit():
-        user=User()
-        # Login and validate the user.
-        # user should be an instance of your `User` class
-        login_user(user)
-        flash('Logged in successfully.')
-        session['username'] = request.form['username']
-        next = request.args.get('next')
-        return redirect(next or url_for('index'))
+        try:
+            user =User.get(User.user_username == form.username.data)
+            if user.user_password==form.password.data:
+                user.is_authenticated = True
+                login_user(user)
+                session['username'] = request.form['username']
+                flash("You're now logged in!")
+                return redirect(url_for('index'))
+            else:
+                flash("Error on password or username")
+        except:
+            flash("Error !!,You should start again")
     return render_template('login.html', form=form)
     
+
 
 @app.route('/signup',methods=['GET','POST'])
 def signup():
@@ -84,7 +95,7 @@ def signup():
     if form.validate_on_submit():
         form.populate_obj(user)
         user.save(force_insert=True)
-        flash('Votre compte est crée')
+        flash('Your account are been created')
         return redirect(url_for('dashboard'))
     return render_template('index.html', form=form)
 
@@ -96,12 +107,10 @@ def logout():
     return render_template('base.html')
 
 
-
-
-
-
-
-
+@login_manager.unauthorized_handler
+def unauthorized():
+    # do stuff
+    return "Impossible !! You must first login to access it"
 
 # FONCTIONS #
 
@@ -118,15 +127,6 @@ def dropdb():
     click.echo('Dropped tables from database')
 
 
-
-
-
-@app.errorhandler(404)
-def notfound():
-    """Serve 404 template."""
-    return make_response(render_template("404.html"), 404)
-
-app.debug = True
-
 if __name__=='__main__':
     app.run()
+    app.debug = True
--- test/models.py
+++ test/models.py
@@ -5,26 +5,42 @@ import wtforms
 from wtfpeewee.orm import model_form
 
 
-
-
-
 database = SqliteDatabase("myfeed.sqlite3")
+
+
+
 
 class BaseModel(Model):
     class Meta:
         database = database
 
 class User(BaseModel,UserMixin):
-    user_id=AutoField(primary_key=True)
+
+    user_id=IntegerField(primary_key=True)
     user_username = CharField(null=True)
     user_password = CharField(null=True)
 
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+    def is_authenticated(self):
+        return True
+
+    def get_username(self):
+        return self.user_id
+
+    def get_id(self):
+        return self.user_id
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
   
     
 class feed(BaseModel):
     feed_nom=CharField(null=True)
-    feed_url=CharField(null=False)
-    feed_date = DateTimeField(default=datetime.datetime.now)
+    feed_url=CharField(null=True)
+    feed_date = DateTimeField(default=datetime.datetime.now,null=True)
     user_feed = ForeignKeyField(User, backref="User")
 
 
@@ -36,4 +52,4 @@ def create_tables():
 
 def drop_tables():
     with database:
-        database.drop_tables([User])
+        database.drop_tables([feed])
